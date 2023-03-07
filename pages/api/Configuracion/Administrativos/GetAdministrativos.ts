ec9205a4ca681512ff413a44b6b8ea89599a30c0
jhonned01 <jhonned01@gmail.com>
2023-03-07 13:18:22 -0500

    deply back

--- pages/api/Configuracion/Administrativos/GetAdministrativos.ts
+++ pages/api/Configuracion/Administrativos/GetAdministrativos.ts
@@ -24,7 +24,7 @@ export default async function handler(
 
     const [AdministrativosRes]: any = await connectionPool.query(
       `select admco.id as Id,concat(admco.admco_nom1,' ',admco.admco_nom2) as Nombre,concat(admco.admco_ape1,' ',admco.admco_ape2) as Apellidos,admco.documento as Documento, admco.cargo as Cargo,admco.mail as Correo,admco.imagen as Imagen,usuario.login Usuario, usuario.pass as Pass,admco.tipo_documento as TipoDocumento,genero as Genero, usuario.rol as TipoUsuario,subSedes.nombre as NombreSubSede from admco INNER JOIN usuario ON(admco.id=usuario.idUsuario)  INNER JOIN subSedes ON subSedes.id=usuario.subsede WHERE usuario.tipo LIKE  'ADMINISTRATIVO' ${
-        SubSede && SubSede != "0" ? `and usuario.subsede = '${SubSede}'` : ""
+        SubSede && SubSede != "0" ? `and usuario.subsede = '${SubSede}' ` : ""
       }
       `
     );
